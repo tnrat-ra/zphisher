fc5d24dfbf1482295ff26a05957b7618c20b30fb
tnrat-ra <75009328+tnrat-ra@users.noreply.github.com>
2020-11-25 12:46:52 +0530

    Update zphisher.sh

--- zphisher.sh
+++ zphisher.sh
@@ -39,7 +39,7 @@ printf "      \e[32;1m/_____| .__/|_| |_|_|___/_| |_|\___|_|   \e[0m\n"
 printf "      \e[33;1m      | |                                \e[0m\n"
 printf "      \e[33;1m      |_|           \e[1;97m    Version 2.1 Beta      \e[0m\n"
 printf "\n"
-printf "   \e[92m[\e[37;1m+\e[92m]\e[0m\e[33;1m Tool Created by htr-tech (tahmid.rayat)\e[0m\n"
+printf "   \e[92m[\e[37;1m+\e[92m]\e[0m\e[33;1m Tool Created by(Prince Yasin)\e[0m\n"
 printf "\n"
 
 }
